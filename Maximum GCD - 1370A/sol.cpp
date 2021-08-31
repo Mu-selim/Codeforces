9f6a6cb278dbdeb69a9e6f78d9e4939e19241ede
Muhammad Selim <89293751+Mu-selim@users.noreply.github.com>
2021-08-31 16:33:14 +0200

    Add files via upload

---
+++ Maximum GCD - 1370A/sol.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include<numeric>
+#include <algorithm>
+#define ll long long int
+using namespace std;
+void Fast()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+}
+
+int main()
+{
+    Fast(); //File_Input();
+    int t; cin >> t;
+    while (t --)
+    {
+        ll n; cin >> n;
+        cout << n/2 << "\n";
+    }
+    return 0;
+}
